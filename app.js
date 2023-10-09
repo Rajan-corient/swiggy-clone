d8eab0eba785c9e05f4606ce8cdbbfc76f408a0d
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2023-10-09 17:25:46 +0530

    parcel added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", {className: "container"},
+const element = React.createElement("div", {className: "container"},
     [React.createElement("h1", {id:"heading"} ,"Hello from react"),
     React.createElement("h2", {id:"heading"} ,"Hello from react h2")]
 );
-
-console.log(heading);
+console.log(element);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);
+root.render(element);
