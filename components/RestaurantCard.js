217ba4bd080e8bc1df9e9ba958e1f5cc6ddf11ff
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2023-10-15 21:12:13 +0530

    retaurant card data binding added

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { imageUrl } from "../utils/constant"
+import { imageUrl, starImageUrl } from "../utils/constant"
+import star from "../assets/star.svg";
 
 const RestaurantCard = ({resData}) => {
   return (
-    <div className="res-card restaurant-item"> 
+    <div className="res-card"> 
+      <div className="offers-container">
         <img className="res-img" src={imageUrl+resData?.cloudinaryImageId} alt="" />
-        <div className="rates">
-          <div className="offers">
-            <span>{resData?.aggregatedDiscountInfoV3?.header}</span>
-            <span>{resData?.aggregatedDiscountInfoV3?.subHeader}</span>
-          </div>
+        <div className="offers">
+          <span>{resData?.aggregatedDiscountInfoV3?.header}</span>
+          <span>{resData?.aggregatedDiscountInfoV3?.subHeader}</span>
         </div>
+      </div>
+      <div className="res-info">
+        <div className="res-name">
+          {resData?.name}
+        </div>
+        <div className="rating">
+          <div> 
+            <img className="star-img" src={starImageUrl} alt="star" /> 
+          </div>
+          <div> {resData?.avgRating} </div>
+        </div>
+        <div className="text-style">{resData?.cuisines.join(", ")}</div>
+        <div className="text-style">{resData?.areaName}</div>
+      </div>
     </div>
   )
 }
--- utils/constant.js
+++ utils/constant.js
@@ -1,8 +1,4 @@
-const restaurantUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1889541&lng=72.835543&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-const imageUrl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const restaurantUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1889541&lng=72.835543&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const imageUrl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const starImageUrl = "https://img.icons8.com/?size=80&id=bqzWt7rkmoX4&format=png";
 
-
-export {
-    restaurantUrl,
-    imageUrl
-}
