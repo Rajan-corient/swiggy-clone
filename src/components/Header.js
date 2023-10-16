76be49530f004b7cf557f4c266056ba2cc47c455
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2023-10-16 20:56:23 +0530

    search restaurant added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/logo.svg";
+import { searchIconUrl } from "../utils/constant";
 
 const Header = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const onRestaurantSearch = (val) => {
+    console.log(val);
+    setSearchText(val);
+  }
+
+
   return (
     <div className="navbar">
       <div className="logo-container">
         <img src={logo} alt="logo" />
       </div>
       <div className="search-container">
-        <input type="text" name="search" id="search" />
+        <div className="input-box">
+          <input 
+          type="text" 
+          name="search" 
+          id="search" 
+          value={searchText}
+          onChange={(e) => {onRestaurantSearch(e.target.value)}}
+          />
+        </div>
+        <div className="search-icon">
+          <img src={searchIconUrl} alt="search" />
+        </div>
       </div>
       <div className="nav-list-container">
         <ul className="nav-list">
